f0fe0d12be4b462dde23161216490b2c74e3f607
Данила Прус <dprus18@gmail.com>
2023-04-18 22:28:38 +0300

    ПервыйДобавление модели Title

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Title(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre, on_delete=models.CASCADE,
+        related_name='titles', blank=True,
+    )
+    catagory = models.ForeignKey(
+        Catagory, on_delete=models.CASCADE,
+        related_name='titles', blank=True,
+    )
+
+    def __str__(self):
+        return self.name
