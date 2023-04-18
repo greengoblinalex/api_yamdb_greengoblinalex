8cf8ab1a4859a24dc7298445a0e434dc5016ef7e
Losharik17 <dimanormanev@yandex.ru>
2023-04-18 23:34:49 +0300

    Небольшой FIX модели Title

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -6,13 +6,21 @@ class Title(models.Model):
     year = models.IntegerField()
     description = models.TextField()
     genre = models.ForeignKey(
-        Genre, on_delete=models.CASCADE,
+        'Genre', on_delete=models.CASCADE,
         related_name='titles', blank=True,
     )
-    catagory = models.ForeignKey(
-        Catagory, on_delete=models.CASCADE,
+    category = models.ForeignKey(
+        'Category', on_delete=models.CASCADE,
         related_name='titles', blank=True,
     )
 
     def __str__(self):
         return self.name
+
+
+class Genre(models.Model):
+    ...
+
+
+class Category(models.Model):
+    ...
