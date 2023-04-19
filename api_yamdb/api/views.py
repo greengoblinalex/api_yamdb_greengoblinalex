0ae6b9f3db6356268cea153a1b4334462ed19f65
Losharik17 <dimanormanev@yandex.ru>
2023-04-19 19:07:24 +0300

    Добавлены вьюсеты для моделей Review и Comment

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from reviews.models import Comment, Review
+from .serializers import (CommentSerializer, ReviewSerializer,)
+
+
+class CommentViewSet(viewsets):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        title_id = self.kwargs['title_id']
+        review_id = self.kwargs['review_id']
+        return Comment.objects.filter(review__id=review_id,
+                                      review__title__id=title_id)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title_id = self.kwargs['title_id']
+        return Review.objects.filter(title__id=title_id)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
