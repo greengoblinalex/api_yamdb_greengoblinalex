77139cc68e54c5d075bb4788389cd70c6032a026
Losharik17 <dimanormanev@yandex.ru>
2023-04-19 19:08:02 +0300

    Добавлены urls для вьюсетов ReviewViewSet и CommentViewSet

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import ReviewViewSet, CommentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
+                'reviews')
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)',
+                CommentViewSet, 'comments')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
