cc2115b50f0d79ba14d28ae620a6da9a5d9aced6
Losharik17 <dimanormanev@yandex.ru>
2023-04-19 20:14:55 +0300

    Перенос моделей из api в reviews

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,10 +1,35 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from api.models import Title
 from .validators import score_validator
 
 User = get_user_model()
+
+
+class Title(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        'Genre', on_delete=models.CASCADE,
+        related_name='titles', blank=True,
+    )
+    category = models.ForeignKey(
+        'Category', on_delete=models.CASCADE,
+        related_name='titles', blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    ...
+
+
+class Category(models.Model):
+    ...
+
 
 
 class Review(models.Model):
