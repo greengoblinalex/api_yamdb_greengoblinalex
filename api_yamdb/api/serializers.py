1c2a437c330ad9e12603b98a592a7171cb5d8b61
Losharik17 <dimanormanev@yandex.ru>
2023-04-19 20:04:19 +0300

    FIX pub_date field

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.fields import DateTimeField, CharField
 from rest_framework.validators import UniqueTogetherValidator
 
 from reviews.models import Comment, Review
 
 
-class ReviewSerializer(serializers.Serializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+class ReviewSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True,
+                              default=serializers.CurrentUserDefault())
+    pub_date = DateTimeField(read_only=True)
 
     class Meta:
         model = Review
-        fields = '__all__'
+        exclude = ('title',)
         validators = [
             UniqueTogetherValidator(
                 queryset=Review.objects.all(),
@@ -19,8 +22,10 @@ class ReviewSerializer(serializers.Serializer):
         ]
 
 
-class CommentSerializer(serializers.Serializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True,
+                              default=serializers.CurrentUserDefault())
+    pub_date = DateTimeField(read_only=True)
 
     class Meta:
         model = Comment
