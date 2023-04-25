e001749e7980d45502c832e4873d94cfe998af99
Losharik17 <dimanormanev@yandex.ru>
2023-04-25 23:26:11 +0300

    Доработка отзывов и коментариев

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,13 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
-from reviews.models import Comment, Review
+from reviews.models import Comment, Review, Title
 from .serializers import (CommentSerializer, ReviewSerializer,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    # permission_classes = (AuthorOrReadOnly,)
 
     def get_queryset(self):
         title_id = self.kwargs['title_id']
@@ -14,15 +16,24 @@ class CommentViewSet(viewsets.ModelViewSet):
                                       review__title__id=title_id)
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        title_id = self.kwargs['title_id']
+        title = get_object_or_404(Title, id=title_id)
+        review_id = self.kwargs['review_id']
+        review = get_object_or_404(title.reviews, id=review_id)
+
+        serializer.save(author=self.request.user, review=review)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
+    # permission_classes = (AuthorOrReadOnly,)
 
     def get_queryset(self):
         title_id = self.kwargs['title_id']
         return Review.objects.filter(title__id=title_id)
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        title_id = self.kwargs['title_id']
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user,
+                        title=title)
