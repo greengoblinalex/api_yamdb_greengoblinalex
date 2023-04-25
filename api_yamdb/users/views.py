cba234bdf360f2ac368212c2906066926a9f520a
Алексей Симонов <greengoblinalex@mail.ru>
2023-04-25 10:45:48 +0300

    Обновление доступа для методов users.views.UserViewset

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import UserSerializer, User
@@ -13,21 +14,29 @@ class UserViewset(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = (IsAdminOrYourself,)
     pagination_class = PageNumberPagination
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
     lookup_field = 'username'
-    filterset_fields = ('username',)
+    search_fields = ('username',)
+    ordering = ('username',)
 
     def get_object(self):
-        if self.kwargs['username'] == 'me':
+        if self.kwargs.get('username') == 'me':
             return self.request.user
         return super().get_object()
 
-    def destroy(self, request, *args, **kwargs):
-        if self.kwargs['username'] == 'me':
-            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        return super().destroy(request, *args, **kwargs)
+    def create(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        if User.objects.filter(email=email).first():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        if self.kwargs['username'] != 'me':
+        if self.kwargs.get('username') != 'me' and request.method == 'PUT':
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        if self.kwargs.get('username') == 'me':
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
