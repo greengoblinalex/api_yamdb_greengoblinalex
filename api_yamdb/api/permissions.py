ee93f53da42c9914f6bc33aefbc2c2bfc88830cb
greengoblinalex <86243539+greengoblinalex@users.noreply.github.com>
2023-04-28 18:24:39 +0300

    Убрал print-ы

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,9 +6,6 @@ class IsAuthorOrModerOrAdminOrReadOnly(permissions.BasePermission):
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        print(request.method in permissions.SAFE_METHODS
-                or (request.user.role in ['admin', 'moderator'])
-                or obj.author == request.user)
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.role in ['admin', 'moderator'])
                 or obj.author == request.user)
@@ -16,7 +13,5 @@ class IsAuthorOrModerOrAdminOrReadOnly(permissions.BasePermission):
 
 class IsAdminRoleOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        print(request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and request.user.role == 'admin'))
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and request.user.role == 'admin'))
