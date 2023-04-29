58f4242c640c94e592d274daae96644bd61e81f9
Losharik17 <dimanormanev@yandex.ru>
2023-04-29 18:00:08 +0300

    FIX permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -36,14 +36,14 @@ class IsAuthor(permissions.BasePermission):
         return bool(obj.author == request.user)
 
 
-class IsSuperuser(permissions.BasePermission):
+class IsSuperuser(DoubledPermission):
     def has_permission(self, request, view):
         return bool(request.user.is_authenticated
                     and request.user.is_superuser)
 
 
-class IsYourself(permissions.BasePermission):
-    def has_object_permission(self, request, view):
+class IsYourself(DoubledPermission):
+    def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
 
