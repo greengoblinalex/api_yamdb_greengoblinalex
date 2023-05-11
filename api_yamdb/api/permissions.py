68f45d063949ff211946cf90c9ba6ebe0236dd0a
Алексей Симонов <greengoblinalex@mail.ru>
2023-05-11 16:19:40 +0300

    Fast fixed permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,15 +6,24 @@ class ReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin
 
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_moder
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
 
 
 class IsAuthor(BasePermission):
